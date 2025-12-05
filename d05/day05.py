b0dd06a52422ad23c0d153483a8eb16d220dd523
Francisco A. Vargas <fvargascanovas@theknotww.com>
2025-12-05 15:20:13 +0100

    Day 5 - Part 2

--- d05/day05.py
+++ d05/day05.py
@@ -1,26 +1,45 @@
 class Day05:
     def __init__(self):
-        self.fresh_ingredients = []
+        self.fresh_ingredients = 0
         self.fresh_ranges = []
-        self.stored_ingredients = []
     
     def process_storage(self):
-        for ingredient in self.stored_ingredients:
-            passed = False
-            for fresh_range in self.fresh_ranges:
-                if not passed and ingredient in range(fresh_range[0], fresh_range[1]+1):
-                    self.fresh_ingredients.append(ingredient)
-                    passed = True
-        print(f"{len(self.fresh_ingredients)} are fresh in storage.")
+        for (i, first) in enumerate(self.fresh_ranges):
+            for j in range(i + 1, len(self.fresh_ranges)):
+                if (i == len(self.fresh_ranges) - 1):
+                    continue
+
+                a, b = first[0], first[1]
+                c, d = self.fresh_ranges[j][0], self.fresh_ranges[j][1]
+
+                overlap = (a <= d) and (c <= b)
+
+                if not overlap:
+                    continue
+
+                overlap_start = max(a, c)
+                overlap_end = min(b, d)
+
+                if overlap_start > c:
+                    self.fresh_ranges.append((c, overlap_start - 1))
+
+                if overlap_end < d:
+                    self.fresh_ranges.append((overlap_end + 1, d))
+
+                self.fresh_ranges[j] = (0,0) # delete but keep the position in list
+               
+        for fresh_range in self.fresh_ranges:
+            if fresh_range[0] != 0:
+                self.fresh_ingredients += (fresh_range[1] - fresh_range[0] + 1)
+        
+        print(f"{self.fresh_ingredients} fresh ingredient IDs")
 
     def process_file(self, file):
         for line in file:
             if "-" in line:
                 line_range = line.split("-")
                 self.fresh_ranges.append((int(line_range[0]), int(line_range[1])))
-            else:
-                if line != "":
-                    self.stored_ingredients.append(int(line))
+        self.fresh_ranges.sort()
 
     def load_test_file(self):
         with open("test.dat", "r") as f:
@@ -34,7 +53,6 @@ class Day05:
 
     def print_fresh_ids(self):
         print(self.fresh_ranges)
-        print(self.stored_ingredients)
 
 day05 = Day05()
 day05.load_file()

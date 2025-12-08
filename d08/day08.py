f73c1deffb4096a0761911209815e821c0bb3177
Francisco A. Vargas <fvargascanovas@theknotww.com>
2025-12-08 16:58:49 +0100

    Day 8 - Part 2

--- d08/day08.py
+++ d08/day08.py
@@ -31,11 +31,6 @@ class Day08:
                 x, y, z = line.split(",")
                 self.nodes.append(Node(int(x), int(y), int(z), len(lines)))
     
-    def print_nodes(self):
-        for (i, node) in enumerate(self.nodes):
-            print(i, end=": ")
-            node.print()
-    
     def calculate_distances(self):
         for i in range(len(self.nodes)):
             for j in range(len(self.nodes)):
@@ -51,36 +46,47 @@ class Day08:
         return shortest
 
     def all_nodes_connected(self):
-        shortest = day08.get_shortest_link()
-        return shortest["distance"] == math.inf
+        return len(self.circuits) == 1 and (len(self.circuits) > 0 and len(self.circuits[0]) == len(self.nodes))
+
+    def connect_circuits_level_1(self):
+        for _ in range(10 if self.environment == TEST else 1000):
+            self.connect_circuits()
+        sorted_list = sorted(day08.circuits, key=len, reverse=True)
+        print(f"Solution for level 1: {reduce(lambda acc, curr: acc * len(curr), sorted_list[:3], 1)}")
+
+    def connect_circuits_level_2(self):
+        shortest = []
+        while not self.all_nodes_connected():
+            shortest = self.connect_circuits()
+        print(f"Solution for level 2: {self.nodes[shortest["origin"]].x * self.nodes[shortest["end"]].x}")
 
     def connect_circuits(self):
-        for _ in range(10 if self.environment == TEST else 1000):
-            shortest = self.get_shortest_link()
+        shortest = self.get_shortest_link()
 
-            self.nodes[shortest["origin"]].distances[shortest["end"]] = math.inf
-            self.nodes[shortest["end"]].distances[shortest["origin"]] = math.inf
+        self.nodes[shortest["origin"]].distances[shortest["end"]] = math.inf
+        self.nodes[shortest["end"]].distances[shortest["origin"]] = math.inf
 
-            already_connected = []
-            for circuit in self.circuits:
-                if shortest["origin"] in circuit:
-                    already_connected.append(circuit)
-                    circuit.add(shortest["end"])
-                elif shortest["end"] in circuit:
-                    already_connected.append(circuit)
-                    circuit.add(shortest["origin"])
-            if len(already_connected) == 0:
-                self.circuits.append(set([shortest["origin"], shortest["end"]]))
-            elif len(already_connected) == 2:
-                merged = already_connected[0].union(already_connected[1])
-                self.circuits.append(merged)
-                self.circuits.remove(already_connected[0])
-                self.circuits.remove(already_connected[1])
+        already_connected = []
+        for circuit in self.circuits:
+            if shortest["origin"] in circuit:
+                already_connected.append(circuit)
+                circuit.add(shortest["end"])
+            elif shortest["end"] in circuit:
+                already_connected.append(circuit)
+                circuit.add(shortest["origin"])
+        if len(already_connected) == 0:
+            self.circuits.append(set([shortest["origin"], shortest["end"]]))
+        elif len(already_connected) == 2:
+            merged = already_connected[0].union(already_connected[1])
+            self.circuits.append(merged)
+            self.circuits.remove(already_connected[0])
+            self.circuits.remove(already_connected[1])
+        return shortest
 
 day08 = Day08(REAL)
 day08.load_list()
 day08.calculate_distances()
-day08.connect_circuits()
+day08.connect_circuits_level_1()
+day08.connect_circuits_level_2()
 
-sorted_list = sorted(day08.circuits, key=len, reverse=True)
-print(f"Solution: {reduce(lambda acc, curr: acc * len(curr), sorted_list[:3], 1)}")
+
